f29c8104e6e683f9301d7e178a1f66e116edbdbb
Ron Sims II <ronsims2@gmail.com>
2021-07-26 21:24:01 -0400

    Added flash func.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from src import microdot as md
+import time
+from machine import Pin
 
+led_flash = Pin(4, Pin.OUT)
 app = md.Microdot()
 
 @app.route('/')
@@ -7,4 +10,12 @@ def index(req):
 	return 'hello world'
 	
 
+def blink_flash(t=0):
+	led_flash.value(0)
+	led_flash.value(1)
+	time.sleep(t)
+	led_flash.value(0)
+	
+	
+blink_flash(1)
 app.run(port=80)
