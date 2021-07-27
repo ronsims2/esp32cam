92d0121345b3b77f33d8c87eef53c203156a562d
Ron Sims II <ronsims2@gmail.com>
2021-07-26 21:25:57 -0400

    Added blink to default request.

--- main.py
+++ main.py
@@ -5,16 +5,19 @@ from machine import Pin
 led_flash = Pin(4, Pin.OUT)
 app = md.Microdot()
 
-@app.route('/')
-def index(req):
-	return 'hello world'
-	
 
 def blink_flash(t=0):
 	led_flash.value(0)
 	led_flash.value(1)
 	time.sleep(t)
 	led_flash.value(0)
+
+
+@app.route('/')
+def index(req):
+	blink_flash(1)
+	return 'hello world'
+	
 	
 	
 blink_flash(1)
