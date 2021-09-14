af38a282cc444f088b78c7053219491f01631ca4
Ron Sims II <ronsims2@gmail.com>
2021-09-13 21:50:40 -0400

    Built a working simple server, integrate a lock file to prevent more than one from being instatiated.

--- httpico.py
+++ httpico.py
@@ -1,26 +1,47 @@
-import socket
+import usocket
 
 
-_httpico = None
+def get_httpico(address, port, timeout):
 
-def get_httpico(address, port):
-    if _httpico is not None:
-        return _httpico
-        
-    
-    s = socket.socket()
-    s.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind(address, port)
+    s = usocket.socket()
+    s.setsockopt(usocket.SOL_SOCKET, usocket.SO_REUSEADDR, 1)
+    s.settimeout(timeout)
+    s.bind((address, port))
     s.listen(1)
     
+    
     _httpico = {
-        'address': address
+        'address': address,
         'port': port,
-        'socket' = s
+        'socket': s
     }
     
     return _httpico
+
+
+def start_server(_httpico, callback):
+    if _httpico is None:
+        return False
         
+    while True:
+        client_conn, client_addr = _httpico['socket'].accept()
+        req = client_conn.recv(1024).decode()
+        
+        print(req)
+        
+        if callback is None:
+            resp = 'HTTP/1.0 500 OK\n\nNo route handler specified.'
+        else:
+            filtered = callback('HTTP/1.0 200 OK\n\nHello World')
+            client_conn.sendall(filtered)
+            client_conn.close()
         
 
+
+def httpico_callback(payload):
+    return payload
+
+
+
+h = get_httpico('192.168.50.151', 8080, 360)
+start_server(h, httpico_callback)
